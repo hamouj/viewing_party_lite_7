9148fc9842477f33ca9481073232c8050e47403a
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-03-21 20:10:14 -0700

    Feat: complete dashboard:discover movies functionality and tests

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -3,32 +3,34 @@
 # spec/features/users/show_spec.rb
 require 'rails_helper'
 
-describe 'As a user' do
+describe 'As a user', type: :feature do
   before(:each) do
     @user1 = create(:user)
     @user2 = create(:user)
+
+    visit user_path(@user1)
   end
 
-  context "When I visit '/users/:id" do
+  context "When I visit '/users/:id'" do
     it "I see '<user name>'s Dashboard' at the top of the page" do
-      visit user_path(@user1)
-
       expect(page).to have_content("#{@user1.name}'s Dashboard")
       expect(page).to_not have_content(@user2.name)
     end
 
     it 'I see a button to Discover Movies' do
-      visit user_path(@user1)
-
       expect(page).to have_button('Discover Movies')
     end
 
     it 'I see a section that lists viewing parties' do
-      visit user_path(@user1)
-
       within '#viewing_parties' do
         expect(page).to have_content('Viewing Parties')
       end
+    end
+
+    it 'When I click on the Discover Movies button, I am redirected to a discover page' do
+      click_button 'Discover Movies'
+
+      expect(current_path).to eq(user_discover_index_path(@user1))
     end
   end
 end
